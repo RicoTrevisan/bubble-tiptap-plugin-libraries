ac30d13b4694903032df6389b13d0b6ce8a56bfe
Rico <r@rico.wtf>
2024-03-08 07:41:19 +0100

    enabled liveblocks

--- index.js
+++ index.js
@@ -1,70 +1,70 @@
-import { Editor } from '@tiptap/core'
+import { Editor } from "@tiptap/core";
 window.tiptapEditor = Editor;
 
 // import StarterKit from '@tiptap/starter-kit'
 // window.tiptapStarterKit = StarterKit;
 
-import Document from '@tiptap/extension-document';
+import Document from "@tiptap/extension-document";
 window.tiptapDocument = Document;
 
-import HardBreak from '@tiptap/extension-hard-break';
+import HardBreak from "@tiptap/extension-hard-break";
 window.tiptapHardBreak = HardBreak;
 
-import Paragraph from '@tiptap/extension-paragraph';
+import Paragraph from "@tiptap/extension-paragraph";
 window.tiptapParagraph = Paragraph;
 
-import Text from '@tiptap/extension-text';
+import Text from "@tiptap/extension-text";
 window.tiptapText = Text;
 
-import Heading from '@tiptap/extension-heading';
+import Heading from "@tiptap/extension-heading";
 window.tiptapHeading = Heading;
 
-import Bold from '@tiptap/extension-bold';
+import Bold from "@tiptap/extension-bold";
 window.tiptapBold = Bold;
 
-import Code from '@tiptap/extension-code';
+import Code from "@tiptap/extension-code";
 window.tiptapCode = Code;
 
-import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight';
+import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
 window.tiptapCodeBlockLowlight = CodeBlockLowlight;
 
-import { lowlight } from 'lowlight';
-window.tiptapLowlight = lowlight;
+// import { lowlight } from 'lowlight';
+// window.tiptapLowlight = lowlight;
 
-import Italic from '@tiptap/extension-italic';
+import Italic from "@tiptap/extension-italic";
 window.tiptapItalic = Italic;
 
-import Strike from '@tiptap/extension-strike';
+import Strike from "@tiptap/extension-strike";
 window.tiptapStrike = Strike;
 
-import Dropcursor from '@tiptap/extension-dropcursor';
+import Dropcursor from "@tiptap/extension-dropcursor";
 window.tiptapDropcursor = Dropcursor;
 
-import Gapcursor from '@tiptap/extension-gapcursor';
-window.tiptapGapcursor = Gapcursor
+import Gapcursor from "@tiptap/extension-gapcursor";
+window.tiptapGapcursor = Gapcursor;
 
-import History from '@tiptap/extension-history';
-window.tiptapHistory = History
+import History from "@tiptap/extension-history";
+window.tiptapHistory = History;
 
-import Blockquote from '@tiptap/extension-blockquote';
+import Blockquote from "@tiptap/extension-blockquote";
 window.tiptapBlockquote = Blockquote;
 
-import BulletList from '@tiptap/extension-bullet-list';
+import BulletList from "@tiptap/extension-bullet-list";
 window.tiptapBulletList = BulletList;
 
-import CodeBlock from '@tiptap/extension-code-block';
+import CodeBlock from "@tiptap/extension-code-block";
 window.tiptapCodeBlock = CodeBlock;
 
-import HorizontalRule from '@tiptap/extension-horizontal-rule';
+import HorizontalRule from "@tiptap/extension-horizontal-rule";
 window.tiptapHorizontalRule = HorizontalRule;
 
-import ListItem from '@tiptap/extension-list-item';
+import ListItem from "@tiptap/extension-list-item";
 window.tiptapListItem = ListItem;
 
-import OrderedList from '@tiptap/extension-ordered-list';
+import OrderedList from "@tiptap/extension-ordered-list";
 window.tiptapOrderedList = OrderedList;
 
-import CharacterCount from '@tiptap/extension-character-count';
+import CharacterCount from "@tiptap/extension-character-count";
 window.tiptapCharacterCount = CharacterCount;
 
 import Youtube from "@tiptap/extension-youtube";
@@ -115,14 +115,20 @@ window.tiptapHighlight = Highlight;
 import { generateHTML } from "@tiptap/html";
 window.tiptapgenerateHTML = generateHTML;
 
-import Collaboration from '@tiptap/extension-collaboration';
+import Collaboration from "@tiptap/extension-collaboration";
 window.tiptapCollaboration = Collaboration;
 
-import CollaborationCursor from '@tiptap/extension-collaboration-cursor';
+import CollaborationCursor from "@tiptap/extension-collaboration-cursor";
 window.tiptapCollaborationCursor = CollaborationCursor;
 
-import { TiptapCollabProvider } from '@hocuspocus/provider';
+import { TiptapCollabProvider } from "@hocuspocus/provider";
 window.tiptapCollabProvider = TiptapCollabProvider;
 
-import * as Y from 'yjs'
+import * as Y from "yjs";
 window.tiptapY = Y;
+
+import { createClient } from "@liveblocks/client";
+window.tiptapCreateClient = createClient;
+
+import LiveblocksProvider from "@liveblocks/yjs";
+window.tiptapLiveblocksProvider = LiveblocksProvider;
